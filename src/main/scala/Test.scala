9601ffe37b65c036afd1c4594dde3a4ee79423bd
gursharan1512 <35790446+gursharan1512@users.noreply.github.com>
2018-10-23 15:13:03 +0530

    Update Test.scala

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,5 +1,5 @@
 object Test {
   def main(args : Array[String]): Unit = {
-    new SendMail().sendMail("sharanghotra1512@gmail.com", "kapil@chutiya","sharanghotra15121512@gmail.com", "SubJect3", "sefawerfdsfsdffMessage")
+    new SendMail().sendMail("sharanghotra1512@gmail.com", "password","sharanghotra15121512@gmail.com", "SubJect3", "sefawerfdsfsdffMessage")
   }
 }
